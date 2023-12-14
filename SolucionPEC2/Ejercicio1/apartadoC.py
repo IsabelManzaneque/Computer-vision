2a2cfd9fd36d9b3b095c44852e6d2d3213eb789d
IsabelM <Isabelvmn93@gmail.com>
2023-12-14 17:15:08 +0000

    empezando ejercicio2

--- SolucionPEC2/Ejercicio1/apartadoC.py
+++ SolucionPEC2/Ejercicio1/apartadoC.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Dec 13 08:40:22 2023
+File: apartadoE.py
+Author: Isabel Manzaneque
+Date: 13/12/2023 
+Description: Segmentación con conocimiento del dominio.
 
-@author: Isabe
 """
 
 import cv2
@@ -65,27 +67,33 @@ def segment(rotatedImg):
   
     grayImg = cv2.cvtColor(rotatedImg.copy(),cv2.COLOR_BGR2GRAY)   
     #thresh = cv2.threshold(grayImg, 200, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]   
-    thresh = cv2.threshold(grayImg, 170, 255, cv2.THRESH_BINARY_INV)[1]    
-    #thresh = cv2.threshold(grayImg, 170, 255, cv2.THRESH_BINARY)[1]  
+    #thresh = cv2.threshold(grayImg, 170, 255, cv2.THRESH_BINARY_INV)[1]    
+    #thresh = cv2.threshold(grayImg, 170, 255, cv2.THRESH_BINARY)[1]      
     
-      
+    # si usas esto no uses threshold y pasale edges a findContours
+    blurredImg = cv2.GaussianBlur(grayImg, (5, 5), 0)
+    edges = cv2.Canny(blurredImg, 50, 150)
+                      
+                      
     # Deteccion de contornos
-    contours = cv2.findContours(thresh, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)[0]
+    contours, h = cv2.findContours(edges, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
     
     # Eliminar los contornos de la bounding box
     filteredContours = []
-    for contour in contours:
+    filteredh = []
+    
+    for i, contour in enumerate(contours):
         rect = cv2.minAreaRect(contour)
         (width, height) = rect[1]
         
         # 100 es un threshold de tamano
         if max(width, height) < 100:
             filteredContours.append(contour)    
-       
+            filteredh.append(h[0][i][3])
     # Devuelve y dibuja los contornos para visualizacion
     contouredImg = cv2.drawContours(rotatedImg.copy(), filteredContours, -1, (0,255,0), 3)
     
-    return contouredImg, filteredContours
+    return contouredImg, filteredContours, filteredh
 
 
 
@@ -115,44 +123,47 @@ def scaleContour(contour, scaleFactorX, scaleFactorY):
     
     return contour
 
-def Normalization1(contours) :
-    
+def Normalization1(contours, j) :
+    """
+    Estire la dimensión más estrecha de la letra para que la 
+    dimensión final sea de 20x20.
+
+    """
     imgCopy = img.copy()
     # Crear una imagen en blanco del mismo tamaño que la imagen original
     height, width = imgCopy.shape[:2]
     white_background = np.ones((height, width, 3), dtype=np.uint8) * 255
     
-     
-    xOffset, yOffset = 0,0
+    #contours = sorted(contours, key=lambda c: cv2.boundingRect(c)[0])
+    
     for contour in contours:
         
+
         # rectángulo delimitador del contorno
         x, y, w, h = cv2.boundingRect(contour)
-        
+
         # Calcular escala en x e y
         scaleFactorX = 20 / w
         scaleFactorY = 20 / h
         
         # escalar contorno a 20x20 y anadir el desplazamiento
         scaledContour = scaleContour(contour, scaleFactorX, scaleFactorY) 
-        scaledContour = scaledContour + (xOffset, yOffset)
+        #scaledContour = scaledContour + (xOffset, yOffset)
         
         # rectangulo del nuevo contorno
         scaledX, scaledY, scaledW, scaledH = cv2.boundingRect(scaledContour)  
-       
-        # dibujar el contorno
-        cv2.drawContours(white_background, [scaledContour], -1, (0, 0, 0), 1)
-        
-          
-        # actualizar el desplazamiento
-        
-        xOffset, yOffset = 5, 0
+        cv2.drawContours(white_background, [scaledContour], -1, (0, 0, 0), 1)         
+            
         
     return white_background
 
 
 def Normalization2(contours) :
-    
+    """
+    Ajusta la escala para que la dimensión mayor de la letra sea de 20 píxeles 
+    y rellene el espacio en la otra dimensión para que el tamaño final sea de 
+    20x20 y la letra esté centrada.
+    """
     imgCopy = img.copy()
     # Crear una imagen en blanco del mismo tamaño que la imagen original
     height, width = imgCopy.shape[:2]
@@ -165,16 +176,44 @@ def Normalization2(contours) :
     # Mostrar la imagen
     
     return white_background
-        
 
+
+
+   
+def Normalization3(rotatedImg, contours) :     
+    
+    imgCopy = rotatedImg.copy()
+    height, width = imgCopy.shape[:2]
+    white_background = np.ones((height, width, 3), dtype=np.uint8) * 255
+    
+    
+    for contour in contours:
+        
+        
+        x, y, w, h = cv2.boundingRect(contour)
+        imgCrop = imgCopy[y:y+h, x:x+w]
+        
+        scaleFactorX = 20 / w
+        scaleFactorY = 20 / h
+        
+        imgResized = cv2.resize(imgCrop, (0,0), fx=scaleFactorX, fy=scaleFactorY)  
+        
+        # Calculate the position to place imgResized (e.g., centering it within the bounding box)
+        offsetX = x + (w - imgResized.shape[1]) // 2
+        offsetY = y + (h - imgResized.shape[0]) // 2
+
+        # Place imgResized onto white_background
+        white_background[offsetY:offsetY+imgResized.shape[0], offsetX:offsetX+imgResized.shape[1]] = imgResized
+
+    return white_background
 
 houghLinesImg, meanAngle = drawHoughLines()
 rotatedImg = rotate(meanAngle)
 segmentated = segment(rotatedImg)[0]
 contours = segment(rotatedImg)[1]
+h = segment(rotatedImg)[2]
 
 
-#cv2.imshow("SEGMENTATED", segmentated)
-cv2.imshow("NORMALIZATED", Normalization1(contours))
+cv2.imshow("NORMALIZATED", Normalization1(contours, h))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
