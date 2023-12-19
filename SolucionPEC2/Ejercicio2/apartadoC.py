223ea1049fb6f44f1802731c5bfa665dd22f9668
IsabelM <Isabelvmn93@gmail.com>
2023-12-19 09:22:11 +0000

    terminado 2.c

--- SolucionPEC2/Ejercicio2/apartadoC.py
+++ SolucionPEC2/Ejercicio2/apartadoC.py
@@ -43,8 +43,10 @@ if len(good)>MIN_MATCH_COUNT:
     # extract the locations of matched keypoints in both the images.
     src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
     dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-
+   
+    # M contiene las transformaciones para pasar de una imagen a otra
     M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+    
     matchesMask = mask.ravel().tolist()
 
     h,w = img1.shape
@@ -52,9 +54,45 @@ if len(good)>MIN_MATCH_COUNT:
     
     # dst contiene las coordenadas de los vértices transformados
     dst = cv2.perspectiveTransform(pts,M)
+    
+    # Calcular posicion 
+    print("Coordenadas de los vertices de la caja en la escena:")
     for i in range(dst.shape[0]):
-        print(f"Vértice {i+1}: ({dst[i][0][0]}, {dst[i][0][1]})")
+        print(f"Vertice {i+1}: ({dst[i][0][0]}, {dst[i][0][1]})")   
+    
+    
+    # Calcular orientacion
+    
+    # centro de la caja en la escena
+    centro = np.mean(dst, axis=0).astype(int)[0]
+    
+    # trazar linea vertical y horizontal a img2
+    img4 = cv2.cvtColor(img2, cv2.COLOR_GRAY2BGR)    
+    img4 = cv2.line(img4, (0, centro[1]), (img4.shape[1], centro[1]), (0, 255, 0), 2)
+    img4 = cv2.line(img4, (centro[0], 0), (centro[0], img4.shape[0]), (0, 255, 0), 2)
+   
+    # vertices de de img1 en img2
+    pt1 = (dst[0][0][0], dst[0][0][1])
+    pt2 = (dst[1][0][0], dst[1][0][1])
+    pt3 = (dst[2][0][0], dst[2][0][1])
+    pt4 = (dst[3][0][0], dst[3][0][1])
+    
+    # Calcula los puntos medios de cada lado
+    centro1x = int((pt1[0] + pt2[0]) / 2)
+    centro1y = int((pt1[1] + pt2[1]) / 2)
+    centro2x = int((pt3[0] + pt4[0]) / 2)
+    centro2y = int((pt3[1] + pt4[1]) / 2)
+    centro3x = int((pt1[0] + pt4[0]) / 2)
+    centro3y = int((pt1[1] + pt4[1]) / 2)
+    centro4x = int((pt2[0] + pt3[0]) / 2)
+    centro4y = int((pt2[1] + pt3[1]) / 2)
+    
 
+    # orientacion de la caja en la escena
+    img4 = cv2.line(img4, (centro1x, centro1y), (centro2x, centro2y), (0, 0, 255), 2)
+    img4 = cv2.line(img4, (centro3x, centro3y), (centro4x, centro4y), (0, 0, 255), 2)
+     
+    
     img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
 
 else:
@@ -71,10 +109,7 @@ draw_params = dict(matchColor = (0,255,0), # draw matches in green color
 img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
 
 
-
-
-
-cv2.imshow("Matched", img3)
-
+cv2.imshow("Resultado", img3)
+cv2.imshow("Orientacion", img4)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
