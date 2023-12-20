6909a0a07c627950cab8d8d7342af9b754dac566
IsabelM <Isabelvmn93@gmail.com>
2023-12-20 10:11:50 +0000

    terminado 3a

--- SolucionPEC2/Ejercicio3/apartadoA.py
+++ SolucionPEC2/Ejercicio3/apartadoA.py
@@ -11,39 +11,28 @@ import numpy as np
 import cv2
 
 
-img = cv2.imread(".\\assets\\blob.jpg")
-imgGrayCopy = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    
-ret,thresh = cv2.threshold(imgGrayCopy,225,255,0)
-contours,hierarchy = cv2.findContours(thresh, 1, 2)
-circleCounter = 0
-IncompleteCircleCounter = 0
-ellipseCounter = 0
-sideCounter = 0
-porcentajes = []
-
 def isCompleteCircle(contour):
     """
     Detecta si un contorno es un circulo completo
 
-    """
+    """  
+
     global img
+    
     contourArea = cv2.contourArea(contour)
     (x,y),radius = cv2.minEnclosingCircle(contour)
     center = (int(x),int(y))
-    radius = int(radius)
     
-    # Area circulo    
+    # Area circulo 
     circleArea = np.pi*(radius**2)
     
-    # Si la diferencia entre el area del contorno y la del circulo minimo 
-    # es menor al 3.5% minimo, el contorno se considera un círculo.
-    if (circleArea - contourArea <  circleArea * 0.035) :
-        img = cv2.circle(img,center,radius,(0,255,0),2)
+    if circleArea - contourArea < 200:
+        img = cv2.circle(img,center,int(radius),(0,255,0),2)
         return True
-    
-    return False  
+    return False
 
-def isIncompleteCircle(contour):
+
+def getPercentage(contour):
     """
     Evaluar el porcentaje de cuña faltante respecto al circulo completo
     """
@@ -51,8 +40,9 @@ def isIncompleteCircle(contour):
     contourArea = cv2.contourArea(contour)
     perimeter = cv2.arcLength(contour, True)
     (x,y),radius = cv2.minEnclosingCircle(contour)
-    radius = int(radius)
-    circleArea = np.pi*(radius**2)    
+    
+    # Area circulo 
+    circleArea = np.pi*(radius**2)
     
     # Aproximación poligonal del contorno
     epsilon = 0.001 * perimeter
@@ -60,16 +50,15 @@ def isIncompleteCircle(contour):
     cv2.drawContours(img, [approx], 0, (0, 255, 255), 2) 
     
     diferencia = circleArea - contourArea
-    porcentaje = round(((diferencia / circleArea) * 100),2)
-    porcentajes.append(porcentaje)
+    return round(((diferencia / circleArea) * 100),2)
+    
     
     
 def isEllipse(contour):
     """
     Detecta si un contorno es una elipse
 
-    """
-    
+    """    
     global img
     # cv2.fitEllipse requiere que el contorno tenga al menos 5 puntos
     if len(contour) < 5:
@@ -84,8 +73,7 @@ def isEllipse(contour):
 
     if abs(contourArea - ellipseArea) < 0.010 * ellipseArea:
         img = cv2.ellipse(img, ellipse, (0, 0, 255), 2)  
-        return True
-    
+        return True    
     return False
     
 
@@ -93,12 +81,11 @@ def sideCounterFun(contour):
     """
     Evalua el numero de lados rectos del contorno
 
-    """
-    
-    # Calcular el perímetro del contorno
+    """    
+    # calcular el perímetro del contorno
     perimeter = cv2.arcLength(contour, True)
 
-    # Aproximación poligonal del contorno
+    # aproximacion poligonal del contorno
     epsilon = 0.02 * perimeter
     approx = cv2.approxPolyDP(contour, epsilon, True)
     
@@ -110,32 +97,66 @@ def sideCounterFun(contour):
         return len(approx)
     return 0
 
-    
-    
-for i, contour in enumerate(contours):     
-         
-    if isCompleteCircle(contour):
-        circleCounter += 1  
-        ## detectar color y tamano aqui?
-    elif isEllipse(contour):
-        ellipseCounter += 1
-    else:
-        lados = sideCounterFun(contour)
-        if(lados < 7):
-            sideCounter += lados
-        else:
-            IncompleteCircleCounter += 1
-            isIncompleteCircle(contour)
-    
-    
-print("Total circulos completos: ", circleCounter)
-print("Total circulos incompletos: ", IncompleteCircleCounter)
-print("Total Elipses: ", ellipseCounter)
-print("Total Lados: ", sideCounter)   
 
-for i, porcentaje in enumerate(porcentajes):
-    print(f"El porcentaje de cuña que le falta al semicirculo {i+1} es {porcentaje}%")
+img = cv2.imread(".\\assets\\blob.jpg")
+imgGrayCopy = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    
+ret,thresh = cv2.threshold(imgGrayCopy,220,255,0)
+contours,hierarchy = cv2.findContours(thresh, 1, 2)    
+circleCounter = InCircleCounter = ellipseCounter = sideCounter = 0
+
+for i, contour in enumerate(contours):     
     
+    shape = color = cuna = ""
+    
+    M = cv2.moments(contour)
+    area = cv2.contourArea(contour)
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+    pixelColor = img[cy, cx][0]
+    
+    if(pixelColor == 0):
+        color = "negro"
+    elif(pixelColor < 130):
+        color = "gris oscuro"
+    elif(pixelColor < 180):
+        color = "gris medio"
+    else:
+        color = "gris claro"
+        
+    # el contorno de la imagen no nos interesa
+    if area < 5000:            
+        
+        if isCompleteCircle(contour):
+            circleCounter += 1  
+            shape = "circulo"            
+        elif isEllipse(contour):
+            ellipseCounter += 1
+            shape = "elipse"
+        else:
+            lados = sideCounterFun(contour)
+            if(lados < 7):
+                sideCounter += lados
+                if lados == 3:
+                    shape = "triangulo"
+                elif lados == 4:
+                    shape = "cuadrado"
+                else:
+                    shape = "hexagono"
+            else:
+                InCircleCounter += 1
+                sideCounter += 2
+                porcentaje = getPercentage(contour)
+                shape = "circulo incompleto" 
+                cuna = f"Porcentaje que le falta: {porcentaje}%"
+                
+        print(f"Blob {i} - Forma: {shape}, Area: {area}, Color: {color}. {cuna}")
+        cv2.putText(img, str(i), (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0,0,255), 1)
+    
+     
+print("\n\nTotal circulos completos: ", circleCounter)
+print("Total circulos incompletos: ", InCircleCounter)
+print("Total Elipses: ", ellipseCounter)
+print("Total Lados: ", sideCounter)       
     
 cv2.imshow("Blob", img)
 cv2.waitKey(0)
